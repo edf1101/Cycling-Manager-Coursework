5912eba690eeb7b7b945dac4a1faf71711580044
Kit Matthewson <kit.matthewson@gmail.com>
2024-02-28 13:18:46 +0000

    Change 'ID' to 'Id'

--- src/cycling/Race.java
+++ src/cycling/Race.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 
 public class Race {
 
-    private static final HashMap<Integer,Race> races = new HashMap<Integer,Race>();
-    private int myID;
+    private static final HashMap<Integer, Race> races = new HashMap<Integer, Race>();
+    private int myId;
     private String name;
     private String description;
     private final ArrayList<Integer> stageIds = new ArrayList<Integer>(); // list of the stages belonging to this race
@@ -14,60 +14,63 @@ public class Race {
     /**
      * Constructor for the Race class
      *
-     * @param name Name of the race
+     * @param name        Name of the race
      * @param description Description of the race
      */
     public Race(String name, String description) throws InvalidNameException {
         // Check for invalid (rule breaking) name
-        if (name == null || name.length()>30 || name.isEmpty() || name.contains(" ")){
+        if (name == null || name.length() > 30 || name.isEmpty() || name.contains(" ")) {
             throw new InvalidNameException(" name broke naming rules. Length must be 0<length<=30, and no whitespace");
         }
 
         // Set up attributes for the object
-        this.myID = UniqueIDGenerator.calculateUniqueID(races);
+        this.myId = UniqueIdGenerator.calculateUniqueId(races);
         this.name = name;
         this.description = description;
 
         // add the new object to the hashmap of all races
-        races.put(this.myID,this);
+        races.put(this.myId, this);
     }
 
-
     /**
-     * Getter for a race by its ID
-     * @param id the ID to query
+     * Getter for a race by its Id
+     *
+     * @param id the Id to query
      * @return The race object reference
      */
-    public static Race getRaceByID(int id){
+    public static Race getRaceById(int id) {
         return races.get(id);
     }
 
     /**
      * Getter for the stage's name
+     *
      * @return The name of the stage instance
      */
-    public String getName(){
+    public String getName() {
         return name;
     }
 
     /**
-     * Getter for the ID attribute on this Race instance
-     * @return the ID
+     * Getter for the Id attribute on this Race instance
+     *
+     * @return the Id
      */
-    public int getId(){
-        return myID;
+    public int getId() {
+        return myId;
     }
 
     /**
      * Gets a short text description of the race including:
-     * race ID, name, description, the number of stages, and the total length
+     * race Id, name, description, the number of stages, and the total length
+     *
      * @return The formatted descriptor string
      */
-    public String getDetails(){
+    public String getDetails() {
         int numStages = stageIds.size();
         double length = 0.0;
         return String.format("Name: %s, Description: %s, Number of stages: %d, Total length: %.2f",
-                name,description,numStages,length);
+                name, description, numStages, length);
     }
 
     /**
@@ -76,55 +79,57 @@ public class Race {
      * @return The descriptor string
      */
     @Override
-    public String toString(){
+    public String toString() {
         return "Race Class " + getDetails();
     }
 
     /**
      * Removes this Race Instance
      */
-    public void remove(){
+    public void remove() {
         // TODO actually do something
-        //  such as cascade down removing stages + checkpoints
-        //  Then remove it from the hashmap of races
+        // such as cascade down removing stages + checkpoints
+        // Then remove it from the hashmap of races
 
-        races.remove(myID);
+        races.remove(myId);
     }
 
     /**
-     * Getter for the list of stage IDs that belong to this Race
+     * Getter for the list of stage Ids that belong to this Race
      *
-     * @return The stage IDs beloning to this race
+     * @return The stage Ids beloning to this race
      */
-    public ArrayList<Integer> getStageIDs(){
+    public ArrayList<Integer> getStageIds() {
         return stageIds;
     }
 
     /**
-     * Gets the ID of a Race by its name
+     * Gets the Id of a Race by its name
      *
-     * @param name The name of the race to be removed.
-     * @param portalsIDs the list of the raceIds that are contained by this portal instance, so we don't mix up this
-     * with another portals race with a same name
+     * @param name       The name of the race to be removed.
+     * @param portalsIds the list of the raceIds that are contained by this portal
+     *                   instance, so we don't mix up this
+     *                   with another portals race with a same name
      * @return The id of the race with the given name (in the context of the portal)
-     * @throws NameNotRecognisedException When the name has not been found in the system
+     * @throws NameNotRecognisedException When the name has not been found in the
+     *                                    system
      */
-    public static int getIDByName(String name, ArrayList<Integer> portalsIDs) throws NameNotRecognisedException {
+    public static int getIdByName(String name, ArrayList<Integer> portalsIds) throws NameNotRecognisedException {
         // Check the name exists in this system by iterating through
-        // all raceIDs in system and checking name
+        // all raceIds in system and checking name
         boolean foundName = false;
-        int foundID = 0;
-        for(int raceID : portalsIDs){
-            if (Race.getRaceByID(raceID).getName().equals(name)){
+        int foundId = 0;
+        for (int raceId : portalsIds) {
+            if (Race.getRaceById(raceId).getName().equals(name)) {
                 foundName = true;
-                foundID = raceID;
+                foundId = raceId;
                 break;
             }
         }
-        if (!foundName){
-            throw new NameNotRecognisedException("The Race name: "+ name+ ", does not exist in this system");
+        if (!foundName) {
+            throw new NameNotRecognisedException("The Race name: " + name + ", does not exist in this system");
         }
-        return foundID;
+        return foundId;
     }
 
 }
